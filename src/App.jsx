dc225e31a761da6fd5c721d9ff77751fa3d04405
Rahul Bajracharya <rbajracharya@triadsys.com>
2023-11-17 08:00:13 -0500

    My first commit

---
+++ src/App.jsx
@@ -0,0 +1,48 @@
+import { CORE_CONCEPTS } from "./data.js";
+import Header from "./components/Header.jsx";
+import CoreConcept from "./components/CoreConcept.jsx";
+import TabButton from "./components/TabButton.jsx";
+
+function App() {
+
+  function handleSelect() {
+    console.log("Hello World!");
+  };
+
+  return (
+    <div>
+      <Header></Header>
+      <main>
+        <section id="core-concepts">
+          <h2>Time to get started!</h2>
+          <ul>
+            <CoreConcept
+              title={CORE_CONCEPTS[0].title}
+              description={CORE_CONCEPTS[0].description}
+              image={CORE_CONCEPTS[0].image}
+            />
+            <CoreConcept {...CORE_CONCEPTS[1]} />
+            <CoreConcept {...CORE_CONCEPTS[2]} />
+            <CoreConcept {...CORE_CONCEPTS[3]} />
+          </ul>
+        </section>
+
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton onSelect={handleSelect}>Components</TabButton>
+            <TabButton onSelect={handleSelect}>JSX</TabButton>
+            <TabButton onSelect={handleSelect}>Props</TabButton>
+            <TabButton onSelect={handleSelect}>State</TabButton>
+          </menu>
+
+          {/* Dynamic Content on button click */}
+
+          
+        </section>
+      </main>
+    </div>
+  );
+}
+
+export default App;
