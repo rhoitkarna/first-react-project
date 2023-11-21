911d028103899dd7d22320028b40332511a2c24e
Rohit Karna <rkarna@triadsys.com>
2023-11-20 23:27:01 -0500

    useState configured

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
-import { CORE_CONCEPTS } from "./data.js";
+import { useState } from "react";
+
+import { CORE_CONCEPTS, EXAMPLES } from "./data.js";
 import Header from "./components/Header.jsx";
 import CoreConcept from "./components/CoreConcept.jsx";
 import TabButton from "./components/TabButton.jsx";
 
 function App() {
+  const [selectedTopic, setSelectedTopic] = useState("components");
 
-  function handleSelect() {
-    console.log("Hello World!");
-  };
+  function handleSelect(selectedButton) {
+    setSelectedTopic(selectedButton);
+  }
 
   return (
     <div>
@@ -30,16 +33,23 @@ function App() {
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TabButton onSelect={handleSelect}>Components</TabButton>
-            <TabButton onSelect={handleSelect}>JSX</TabButton>
-            <TabButton onSelect={handleSelect}>Props</TabButton>
-            <TabButton onSelect={handleSelect}>State</TabButton>
+            <TabButton onSelect={() => handleSelect("components")}>
+              Components
+            </TabButton>
+            <TabButton onSelect={() => handleSelect("jsx")}>JSX</TabButton>
+            <TabButton onSelect={() => handleSelect("props")}>Props</TabButton>
+            <TabButton onSelect={() => handleSelect("state")}>State</TabButton>
           </menu>
 
           {/* Dynamic Content on button click */}
-          <h3>Dynamic Content</h3>
 
-          
+          <div id="tab-content">
+            <h3>{EXAMPLES[selectedTopic].title}</h3>
+            <p>{EXAMPLES[selectedTopic].description}</p>
+            <pre>
+              <code>{EXAMPLES[selectedTopic].code}</code>
+            </pre>
+          </div>
         </section>
       </main>
     </div>
